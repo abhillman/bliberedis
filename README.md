d7aa77ad519c22896ab944e6c3350882c0ee774d
Aryeh Hillman <aryeh@brilliantlime.com>
2013-12-30 00:20:28 -0800

    Pylint'd! And updated README.

--- README.md
+++ README.md
@@ -3,9 +3,15 @@ Project in early stage. Uses Flickr's API to populate Redis KV-store hashes
 collection of out-of-copyright images. Access to the API is multithreaded
 to facilitate multiple open connections at once for speed.
 
+Execution notes:
+- Be sure to get and set an API key from flickr within populate.py
+- Only gets the first 500 images...
+- Requirements include
+    - running instance of redis
+
 Bugs are:
 - Some errors are not caught (i.e. IOError from socket)
 - Sometimes program does not terminate if there is an uncaught error
 
 Other TODO items:
-- Look at optimizing API access... currently using existing flickr.py, non- multithreaded
+- Look at optimizing API access... currently using existing flickr.py, non-multithreaded
--- populate.py
+++ populate.py
@@ -1,27 +1,43 @@
 #!/usr/bin/env python
+"""
+Small package to pull meta-data (i.e. url's, description)
+about the first 500 images from the British Library's
+public image collection. Places meta-data into a redis
+database using hash structures prepended by "flickr_"
+followed by Flickr's ID for the image.
+
+Hash structures are tracked by an ordered set (in order
+based on APIs order when searching BL's collection)
+called "flickr".
+"""
+
+__author__ = "Aryeh Hillman <aryehbh@gmail.com>"
+__version__ = "0.1"
+__date__ = "December 30, 2013"
+
 import flickr
 import redis
-import simplejson
 import threading
 import Queue
 import time
 from xml.parsers.expat import ExpatError
 import sys
+import itertools
 
 # Set up the Flickr API
 flickr.debug = False
 flickr.API_KEY = 'YOUR_API_KEY_HERE'
 # Save the British Library's NSID
-british_library_nsid = '12403504@N02'
+BRITISH_LIBRARY_NSID = '12403504@N02'
 
 # Set up connection to Redis
-redis_handle = redis.StrictRedis(host='localhost', port=6379, db=0)
+REDIS_HANDLE = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # Other globals
-image_prefix_redis = "flickr_"
-image_set_name_redis = "flickr"
+IMAGE_PREFIX_REDIS = "flickr_"
+IMAGE_SET_NAME_REDIS = "flickr"
 
-def getOriginalData(image):
+def get_original_data(image):
     """Method to get the original
     image url and dimensions from
     a flickr.py image object"""
@@ -36,18 +52,28 @@ def getOriginalData(image):
         sys.stdout.flush()
     return {}
 
-def getDescription(image):
+def get_description(image):
+    """Simply wraps a call to the image's
+    description attribute. We need this because
+    occassionally the flickr package raises
+    an error when XML parsing which we want to catch
+    to continue processing."""
     try:
         description = image.description
+        return description
     except ExpatError:
         #print "Couldn't get description for image %s" % image.id
         sys.stdout.write("_")
         sys.stdout.flush()
     return None
 
-def getPhotoDictionary(photo):
-    original_data = getOriginalData(photo)
-    description = getDescription(photo)
+def get_photo_dictionary(photo):
+    """Creates a Python dictionary representing
+    a photo and returns it. Keys include URLs for
+    square, thumbnail, small, medium, large, and original
+    sizes; width and height; title; id; and description."""
+    original_data = get_original_data(photo)
+    description = get_description(photo)
     photo_dictionary = dict(
             photo_id = photo.id,
             title = photo.title,
@@ -63,14 +89,18 @@ def getPhotoDictionary(photo):
             )
     return photo_dictionary
 
-def photoDictionaryIntoRedis(photo_dictionary):
+def photo_dictionary_into_redis(photo_dictionary):
+    """Takes a dictionary representation of a photo
+    and inserts it into redis under keyname
+    of the photo's id (from the dictionary) prepended
+    by the global IMAGE_PREFIX_REDIS string."""
     # Add the photo dictionary to redis
     photo_id = photo_dictionary.get('photo_id')
-    name = ''.join([image_prefix_redis, photo_id])
-    redis_handle.hmset(name, photo_dictionary)
+    name = ''.join([IMAGE_PREFIX_REDIS, photo_id])
+    REDIS_HANDLE.hmset(name, photo_dictionary)
     # Add a reference to the photo
     # to an ordered set in redis
-    redis_handle.zadd(image_set_name_redis, photo_id, name)
+    REDIS_HANDLE.zadd(IMAGE_SET_NAME_REDIS, photo_id, name)
     return name
 
 class PhotoDictionaryThread(threading.Thread):
@@ -78,14 +108,14 @@ class PhotoDictionaryThread(threading.Thread):
     def __init__(self, photo_queue, photo_dictionary_queue):
         threading.Thread.__init__(self)
         self.photo_queue = photo_queue
-        self.photo_dictionary_queue = photo_dictionary_queue 
-    
+        self.photo_dictionary_queue = photo_dictionary_queue
+
     def run(self):
         while True:
             # Get photo from queue
             photo = self.photo_queue.get()
             # Construct dictionary for photo
-            photo_dictionary = getPhotoDictionary(photo)
+            photo_dictionary = get_photo_dictionary(photo)
             sys.stdout.write('x')
             sys.stdout.flush()
             self.photo_dictionary_queue.put(photo_dictionary)
@@ -96,59 +126,64 @@ class PhotoDictionaryRedisThread(threading.Thread):
     def __init__(self, photo_dictionary_queue):
         threading.Thread.__init__(self)
         self.photo_dictionary_queue = photo_dictionary_queue
-    
+
     def run(self):
         while True:
             # Get photo dictionary from queue
             photo_dictionary = self.photo_dictionary_queue.get()
-            photoDictionaryIntoRedis(photo_dictionary)
+            photo_dictionary_into_redis(photo_dictionary)
             sys.stdout.write('o')
             sys.stdout.flush()
             self.photo_dictionary_queue.task_done()
 
-photo_queue = Queue.Queue()
-photo_dictionary_queue = Queue.Queue()
 def populate_redis():
-    # Get a sense of how many pages of results we are dealing with 
-    # num_pages = int(flickr.photos_search_pages(
-    #                     user_id=british_library_nsid, per_page=500))
-    # We hard code the num_pages here -- don't need to do this
-    num_pages = 2040
+    """Main function of this module. Gets the first
+    500 images from the British Library's public
+    photo collection and stores information about each
+    photo including URLs, title, and description in Redis."""
 
-    #for x in xrange(3, 11):
+    # Get a sense of how many pages of results we are dealing with
+    # num_pages = int(flickr.photos_search_pages(
+    #                     user_id=BRITISH_LIBRARY_NSID, per_page=500))
+
+    photo_queue = Queue.Queue()
+    photo_dictionary_queue = Queue.Queue()
+
     print "Getting image pages..."
-    for x in xrange(30, 40):
-        photos = flickr.photos_search(user_id=british_library_nsid,
+    for page_number in xrange(1, 2):
+        photos = flickr.photos_search(user_id=BRITISH_LIBRARY_NSID,
                                      per_page=500,
-                                     page=x)
+                                     page=page_number)
         # Populate the queue with photos
-        print "Populating our queue of photos for page %d..." % x
+        print "Populating our queue of photos for page %d..." % page_number
         for photo in photos:
             photo_queue.put(photo)
 
     print "Creating pool of threads to process photos..."
     # Create a pool of threads to process photos
-    for i in range(200):
+    for _ in itertools.repeat(None, 200):
         pdt = PhotoDictionaryThread(photo_queue, photo_dictionary_queue)
         pdt.setDaemon(True)
         pdt.start()
 
     print "Creating pool of threads to put photos into redis..."
     # Create a pool of threads to place photos into redis
-    for i in range(20):
+    for _ in itertools.repeat(None, 20):
         pdr = PhotoDictionaryRedisThread(photo_dictionary_queue)
         pdr.setDaemon(True)
         pdr.start()
-    
+
     # Wait on the queues
     print "Waiting for the queues to empty!"
     photo_queue.join()
     photo_dictionary_queue.join()
 
 def timeit():
+    """Calls method to populate redis with images
+    and times the process"""
     start = time.time()
     populate_redis()
     print "Elapsed Time: %s" % (time.time() - start)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     timeit()
